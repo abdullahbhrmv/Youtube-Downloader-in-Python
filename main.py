d44acf5e7f9afe398829e9190b2177b03ccf8ecb
Abdullah Bahromov <80536748+abdullahbhrmv@users.noreply.github.com>
2022-10-08 17:27:10 +0300

    YouTube Downloader with Python

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pytube
+import pytube         # pip install pytube
 
-link = input("YouTube link: ")
-print("Yuklanmoqda...")
-yt = pytube.YouTube(link)
+link = input("YouTube link: ")     # link kiriting
+print("Yuklanmoqda...")    # yuklanmoqda degan xabar beradi
+yt = pytube.YouTube(link)   # linkni yuklash
 
-yt.streams.filter(progressive=True, file_extension='mp4').desc().first().download()
-print("Yuklab olingan fayl: ", link)
+yt.streams.filter(progressive=True, file_extension='mp4').desc().first().download()    # video yuklash
+print("Yuklab olingan fayl: ", link)  # yuklab olingan fayl ko'rsatib beradi
